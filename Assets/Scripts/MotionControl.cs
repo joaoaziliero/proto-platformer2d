c16fa490a82557460f8e8ee7ef2958f1e045cc02
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2023-10-09 23:19:47 -0300

    implemented basic character movements

--- Assets/Scripts/MotionControl.cs
+++ Assets/Scripts/MotionControl.cs
@@ -4,5 +4,40 @@ using UnityEngine;
 
 public class MotionControl : MonoBehaviour
 {
+    public Rigidbody2D character;
 
+    [Header("Motion Settings")]
+    [SerializeField] public float speed;
+    [SerializeField] public float frictionScale;
+    [SerializeField] public float jumpScale;
+
+    private void Update()
+    {
+
+    }
+
+    private void HandleLateralMotion()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            character.velocity = new Vector2(speed, character.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            character.velocity = new Vector2(-speed, character.velocity.y);
+        }
+
+        if (character.velocity.x != 0 && character.velocity.y == 0)
+        {
+            character.velocity -= character.velocity.normalized * frictionScale;
+        }
+    }
+
+    private void HandleUpwardMotion()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            character.velocity = jumpScale * Vector2.up;
+        }
+    }
 }
